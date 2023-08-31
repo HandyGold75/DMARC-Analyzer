4c3eaa265cd1d6afbcd4677b20a145ffbad32a08
= <=>
2023-08-31 15:15:09 +0200

    31-Aug-23

--- dmarcAnalyzer.py
+++ dmarcAnalyzer.py
@@ -11,7 +11,7 @@ from argparse import ArgumentParser
 
 
 class setup:
-    workFolder = f'{osPath.split(__file__)[0]}\\DMARC'
+    workFolder = f"{osPath.split(__file__)[0]}\\DMARC"
 
     domains = []
     mailbox = ""
@@ -23,7 +23,7 @@ class setup:
 
     def arg():
         parser = ArgumentParser(description="Generates an interactive dmarc report pulled from dmarc reports in an Outlook mailbox.")
-        parser.add_argument("-d", "-domains", default="mydomain.com,mydomain.co.uk,anotherdomain.eu", type=str, help="Specify domains to be checked, split with \',\', eq: mydomain.com,mydomain.co.uk,anotherdomain.eu")
+        parser.add_argument("-d", "-domains", default="mydomain.com,mydomain.co.uk,anotherdomain.eu", type=str, help="Specify domains to be checked, split with ',', eq: mydomain.com,mydomain.co.uk,anotherdomain.eu")
         parser.add_argument("-m", "-mailbox", default="DMARC\\Inbox", type=str, help="Specify mailbox where dmarc reports land in, folders can be specified with '\\', eq: DMARC\\Inbox")
         parser.add_argument("-a", "-age", default=31, type=int, help="Specify how old in days reports may be, based on email receive date (31 is default; 0 to disable age filtering).")
         parser.add_argument("-ur", "-unread", action="store_true", help="Only cache unread mails.")
@@ -43,14 +43,14 @@ class setup:
             mkdir(setup.workFolder)
 
         for domain in setup.domains:
-            paths = [f'{setup.workFolder}\\{domain}', f'{setup.workFolder}\\{domain}\\Comp', f'{setup.workFolder}\\{domain}\\Xml', f'{setup.workFolder}\\{domain}\\Done']
+            paths = [f"{setup.workFolder}\\{domain}", f"{setup.workFolder}\\{domain}\\Comp", f"{setup.workFolder}\\{domain}\\Xml", f"{setup.workFolder}\\{domain}\\Done"]
 
             for path in paths:
                 if not osPath.exists(path):
                     mkdir(path)
 
-            if not osPath.exists(f'{setup.workFolder}\\{domain}\\{domain}-report.json'):
-                with open(f'{setup.workFolder}\\{domain}\\{domain}-report.json', "w") as file_W:
+            if not osPath.exists(f"{setup.workFolder}\\{domain}\\{domain}-report.json"):
+                with open(f"{setup.workFolder}\\{domain}\\{domain}-report.json", "w") as file_W:
                     file_W.write("{}")
 
     def saveAttachments():
@@ -66,17 +66,17 @@ class setup:
             try:
                 folder = folder.Folders(i)
             except pywintypes.com_error:
-                exit(f'\nCan\'t open Outlook folder {i}!\nMake sure the script and Outlook are not running with elevated privalages, the target folder is not open in Outlook or try restarting Outlook!\n')
+                exit(f"\nCan't open Outlook folder {i}!\nMake sure the script and Outlook are not running with elevated privalages, the target folder is not open in Outlook or try restarting Outlook!\n")
 
         for message in folder.Items:
             if not message.UnRead and setup.ignoreRead:
                 setup.skipped += 1
-                gui_splash.update(f'Saving attachments: {setup.loaded}\nSkipped: {setup.skipped}')
+                gui_splash.update(f"Saving attachments: {setup.loaded}\nSkipped: {setup.skipped}")
                 continue
 
             if setup.age > 0 and message.ReceivedTime.timestamp() < (datetime.today() - timedelta(days=setup.age)).timestamp():
                 setup.skipped += 1
-                gui_splash.update(f'Saving attachments: {setup.loaded}\nSkipped: {setup.skipped}')
+                gui_splash.update(f"Saving attachments: {setup.loaded}\nSkipped: {setup.skipped}")
                 continue
 
             subjectSplited = str(message.Subject).replace(":", "").split(" ")
@@ -98,8 +98,8 @@ class setup:
                 xmlFileName = str(attachment).replace(".xml", "").replace(".zip", ".xml").replace(".gztar", ".xml").replace(".bztar", ".xml").replace(".tar", ".xml").replace(".gz", ".xml")
 
                 for domain in setup.domains:
-                    compFolder = f'{setup.workFolder}\\{domain}\\Comp'
-                    xmlFolder = f'{setup.workFolder}\\{domain}\\Xml'
+                    compFolder = f"{setup.workFolder}\\{domain}\\Comp"
+                    xmlFolder = f"{setup.workFolder}\\{domain}\\Xml"
 
                     if not "report domain: " in message.Subject.lower() or not domain.lower() in message.Subject.lower():
                         continue
@@ -107,7 +107,7 @@ class setup:
                         continue
 
                     try:
-                        attachment.SaveAsFile(f'{compFolder}\\{attachment}')
+                        attachment.SaveAsFile(f"{compFolder}\\{attachment}")
                     except pywintypes.com_error:
                         print(f'Can\'t save attachment in email "{attachment}" (skipping)!')
                         continue
@@ -115,39 +115,39 @@ class setup:
                     attachment = str(attachment)
 
                     setup.loaded += 1
-                    gui_splash.update(f'Saving attachments: {setup.loaded}\nSkipped: {setup.skipped}')
+                    gui_splash.update(f"Saving attachments: {setup.loaded}\nSkipped: {setup.skipped}")
 
                     if attachment.endswith(".rar") or attachment.endswith(".7z"):
-                        print(f'Rar or 7z file found, unpacking these archives is not yet supported!')
+                        print(f"Rar or 7z file found, unpacking these archives is not yet supported!")
                         continue
 
                     elif attachment.endswith(".zip") or attachment.endswith(".tar"):
-                        unpack_archive(f'{compFolder}\\{attachment}', f'{setup.workFolder}\\{domain}\\Xml\\')
+                        unpack_archive(f"{compFolder}\\{attachment}", f"{setup.workFolder}\\{domain}\\Xml\\")
 
                     elif attachment.endswith(".gz"):
-                        with gopen(f'{compFolder}\\{attachment}', "rt") as fileIn:
-                            with open(f'{xmlFolder}\\{xmlFileName}', "w") as fileOut:
+                        with gopen(f"{compFolder}\\{attachment}", "rt") as fileIn:
+                            with open(f"{xmlFolder}\\{xmlFileName}", "w") as fileOut:
                                 fileOut.write(fileIn.read())
 
                     else:
                         try:
-                            unpack_archive(f'{compFolder}\\{attachment}', f'{setup.workFolder}\\{domain}\\Xml\\')
+                            unpack_archive(f"{compFolder}\\{attachment}", f"{setup.workFolder}\\{domain}\\Xml\\")
                         except ReadError:
-                            with gopen(f'{compFolder}\\{attachment}', "rt") as fileIn:
-                                with open(f'{xmlFolder}\\{xmlFileName}', "w") as fileOut:
+                            with gopen(f"{compFolder}\\{attachment}", "rt") as fileIn:
+                                with open(f"{xmlFolder}\\{xmlFileName}", "w") as fileOut:
                                     try:
                                         fileOut.write(fileIn.read())
                                     except BadGzipFile:
-                                        print(f'Unknown archive, unable to unpack archive! File: {compFolder}\\{attachment}')
+                                        print(f"Unknown archive, unable to unpack archive! File: {compFolder}\\{attachment}")
                                         continue
 
                     try:
-                        rename(f'{xmlFolder}\\{xmlFileName}', f'{xmlFolder}\\{xmlFileName.replace(".xml", "")}!{nameAppend}.xml')
+                        rename(f"{xmlFolder}\\{xmlFileName}", f'{xmlFolder}\\{xmlFileName.replace(".xml", "")}!{nameAppend}.xml')
                     except FileExistsError:
                         remove(f'{xmlFolder}\\{xmlFileName.replace(".xml", "")}!{nameAppend}.xml')
-                        rename(f'{xmlFolder}\\{xmlFileName}', f'{xmlFolder}\\{xmlFileName.replace(".xml", "")}!{nameAppend}.xml')
+                        rename(f"{xmlFolder}\\{xmlFileName}", f'{xmlFolder}\\{xmlFileName.replace(".xml", "")}!{nameAppend}.xml')
 
-                    remove(f'{compFolder}\\{attachment}')
+                    remove(f"{compFolder}\\{attachment}")
 
     def main():
         setup.arg()
@@ -163,14 +163,14 @@ class reportHandel:
         reports = []
 
         for domain in setup.domains:
-            xmlFiles = listdir(f'{setup.workFolder}\\{domain}\\Xml')
+            xmlFiles = listdir(f"{setup.workFolder}\\{domain}\\Xml")
 
             for file in xmlFiles:
-                with open(f'{setup.workFolder}\\{domain}\\Xml\\{file}', "r") as currentFile:
+                with open(f"{setup.workFolder}\\{domain}\\Xml\\{file}", "r") as currentFile:
                     try:
                         currentFile_Dict = xmlParse(currentFile.read())
                     except Exception:
-                        print(f'Unable to read file! File: {setup.workFolder}\\{domain}\\Xml\\{file}')
+                        print(f"Unable to read file! File: {setup.workFolder}\\{domain}\\Xml\\{file}")
                         continue
                     currentFile_Dict["feedback"]["report_metadata"]["filename"] = file
 
@@ -198,7 +198,7 @@ class reportHandel:
                     "spf_domain": [],
                     "spf_check": [],
                     "dkim_domain": [],
-                    "dkim_check": []
+                    "dkim_check": [],
                 }
 
                 if "spf" in record["auth_results"]:
@@ -228,14 +228,14 @@ class reportHandel:
                 "date_range": f'{metaData["date_range"]["begin"]}/{metaData["date_range"]["end"]}',
                 "date_start": datetime.fromtimestamp(int(metaData["date_range"]["begin"])).strftime("%d/%m/%y %H:%M"),
                 "date_end": datetime.fromtimestamp(int(metaData["date_range"]["end"])).strftime("%d/%m/%y %H:%M"),
-                "records": currentRecords
+                "records": currentRecords,
             }
 
         return finalReports
 
     def logReports(reports):
         for i, report in enumerate(reports):
-            gui_splash.update(f'Updating main reports:\n{i}')
+            gui_splash.update(f"Updating main reports:\n{i}")
 
             reportData = reports[report]
 
@@ -252,7 +252,7 @@ class reportHandel:
         mainLog = {}
 
         for domain in setup.domains:
-            with open(f'{setup.workFolder}\\{domain}\\{domain}-report.json', "r") as mainLogFile:
+            with open(f"{setup.workFolder}\\{domain}\\{domain}-report.json", "r") as mainLogFile:
                 mainLogTmp = load(mainLogFile)
 
             for report in mainLogTmp:
@@ -328,12 +328,14 @@ class reportHandel:
 class gui_splash:
     sg.theme("DarkGrey13")
 
-    window_loading = sg.Window("DMARC Analazer",
-                               [[sg.Text("DMARC Analyzer", justification="center", font=("Helvetica", 14), pad=(0, 10), expand_x=True, expand_y=True)], [sg.Text("Loading. . .\n", justification="center", expand_x=True, expand_y=True, key="LoadingText")]],
-                               size=(200, 100),
-                               no_titlebar=True,
-                               keep_on_top=True,
-                               finalize=True)
+    window_loading = sg.Window(
+        "DMARC Analazer",
+        [[sg.Text("DMARC Analyzer", justification="center", font=("Helvetica", 14), pad=(0, 10), expand_x=True, expand_y=True)], [sg.Text("Loading. . .\n", justification="center", expand_x=True, expand_y=True, key="LoadingText")]],
+        size=(200, 100),
+        no_titlebar=True,
+        keep_on_top=True,
+        finalize=True,
+    )
 
     def setup():
         gui_splash.window_loading.refresh()
@@ -361,17 +363,17 @@ class gui_main:
             success_keys = []
 
             for file in reportHandel.reportsSummary[domain]["success_files"]:
-                success_keys.append(f'{file}::Action_OpenDir_{domain}\\Done\\{file}')
+                success_keys.append(f"{file}::Action_OpenDir_{domain}\\Done\\{file}")
 
             spf_keys = []
 
             for file in reportHandel.reportsSummary[domain]["spf_files"]:
-                spf_keys.append(f'{file}::Action_OpenDir_{domain}\\Done\\{file}')
+                spf_keys.append(f"{file}::Action_OpenDir_{domain}\\Done\\{file}")
 
             dkim_keys = []
 
             for file in reportHandel.reportsSummary[domain]["dkim_files"]:
-                dkim_keys.append(f'{file}::Action_OpenDir_{domain}\\Done\\{file}')
+                dkim_keys.append(f"{file}::Action_OpenDir_{domain}\\Done\\{file}")
 
             summary = [
                 sg.Text(f'Count: {reportHandel.reportsSummary[domain]["count"]}', pad=(5, 10)),
@@ -380,15 +382,15 @@ class gui_main:
                 sg.Push(),
                 sg.Text(f'SPF Failed: {reportHandel.reportsSummary[domain]["spf_failed"]} ({((reportHandel.reportsSummary[domain]["spf_failed"] * 100) / reportHandel.reportsSummary[domain]["count"]):.2f}%)', right_click_menu=["", spf_keys], pad=(5, 10)),
                 sg.Push(),
-                sg.Text(f'DKIM Failed: {reportHandel.reportsSummary[domain]["dkim_failed"]} ({((reportHandel.reportsSummary[domain]["dkim_failed"] * 100) / reportHandel.reportsSummary[domain]["count"]):.2f}%)',
-                        right_click_menu=["", dkim_keys],
-                        pad=(5, 10))
+                sg.Text(
+                    f'DKIM Failed: {reportHandel.reportsSummary[domain]["dkim_failed"]} ({((reportHandel.reportsSummary[domain]["dkim_failed"] * 100) / reportHandel.reportsSummary[domain]["count"]):.2f}%)', right_click_menu=["", dkim_keys], pad=(5, 10)
+                ),
             ]
 
             buttons = [
-                sg.Button("Show reports", key=f'Action_ShowHide_{domain}_reports', tooltip="Show/ hide the reports list.", pad=(5, 10)),
-                sg.Button("Json report", key=f'Action_OpenFile_{domain}\\{domain}-report.json', tooltip="Open the Json report in notepad.", pad=(5, 10)),
-                sg.Button("Open dir", key=f'Action_OpenDir_{domain}', tooltip="Open the domain's directory.", pad=(5, 10))
+                sg.Button("Show reports", key=f"Action_ShowHide_{domain}_reports", tooltip="Show/ hide the reports list.", pad=(5, 10)),
+                sg.Button("Json report", key=f"Action_OpenFile_{domain}\\{domain}-report.json", tooltip="Open the Json report in notepad.", pad=(5, 10)),
+                sg.Button("Open dir", key=f"Action_OpenDir_{domain}", tooltip="Open the domain's directory.", pad=(5, 10)),
             ]
 
             reportsList = []
@@ -401,15 +403,18 @@ class gui_main:
                 reportsList.append([sg.Text(report["file"], key=f'Action_OpenDir_{domain}\\Done\\{report["file"]}', enable_events=True, tooltip="Open this report in notepad.")])
 
             reports = [
-                sg.Frame("Reports", [[sg.Column(reportsList, scrollable=True, vertical_scroll_only=True, expand_x=True, size=(None, 200), pad=(0, 0))]],
-                         key=f'ShowHide_Item_{domain}',
-                         visible=False,
-                         title_location="n",
-                         vertical_alignment="top",
-                         expand_x=True,
-                         expand_y=True,
-                         pad=(1, 1)),
-                sg.Image(size=(0, 0), pad=(0, 0))
+                sg.Frame(
+                    "Reports",
+                    [[sg.Column(reportsList, scrollable=True, vertical_scroll_only=True, expand_x=True, size=(None, 200), pad=(0, 0))]],
+                    key=f"ShowHide_Item_{domain}",
+                    visible=False,
+                    title_location="n",
+                    vertical_alignment="top",
+                    expand_x=True,
+                    expand_y=True,
+                    pad=(1, 1),
+                ),
+                sg.Image(size=(0, 0), pad=(0, 0)),
             ]
 
             reportFrameList.append(sg.Frame(domain, [summary, buttons, reports], expand_x=True, expand_y=True, pad=(0, 10)))
@@ -439,7 +444,9 @@ class gui_main:
         ]
 
         helpmenu = [
-            sg.Text(("""
+            sg.Text(
+                (
+                    """
 Before the GUI is launched dmarc reports will be pulled from the Outlook client and processed into an report per domain.
 An summary of every domain's report is visable in this GUI.\n
 The files used by this program are orginized as followed:\n
@@ -448,10 +455,12 @@ DMARC					-->		Main work directory.
     ├───Comp				-->		Folder for pulled compiled files.
     ├───Done				-->		Folder for uncompiled files after being processed.
     ├───Xml				-->		Folder for uncompiled files.
-    └───<Domain>-report.json		-->		Json report of all processed files.\n"""),
-                    visible=False,
-                    key="ShowHide_Item_Help"),
-            sg.Image(size=(0, 0), pad=(0, 0))
+    └───<Domain>-report.json		-->		Json report of all processed files.\n"""
+                ),
+                visible=False,
+                key="ShowHide_Item_Help",
+            ),
+            sg.Image(size=(0, 0), pad=(0, 0)),
         ]
 
         return [header, gui_main.lay_reports(), footer, helpmenu]
@@ -468,33 +477,33 @@ DMARC					-->		Main work directory.
 
     def act_open(folder=None, file=None):
         if not folder is None:
-            Popen(f'explorer {setup.workFolder}\\{folder}')
+            Popen(f"explorer {setup.workFolder}\\{folder}")
 
         elif not file is None:
-            startfile(f'{setup.workFolder}\\{file}')
+            startfile(f"{setup.workFolder}\\{file}")
 
     def act_showHide(button, text):
         gui_main.showHideStates[button] = not gui_main.showHideStates[button]
 
         if gui_main.showHideStates[button]:
-            gui_main.window[f'Action_ShowHide_{button}_{text}'].update(text=f'Hide {text}')
+            gui_main.window[f"Action_ShowHide_{button}_{text}"].update(text=f"Hide {text}")
         else:
-            gui_main.window[f'Action_ShowHide_{button}_{text}'].update(text=f'Show {text}')
+            gui_main.window[f"Action_ShowHide_{button}_{text}"].update(text=f"Show {text}")
 
         if button == "All":
             for domain in gui_main.activeDomains:
                 gui_main.showHideStates[domain] = gui_main.showHideStates["All"]
 
                 if gui_main.showHideStates[domain]:
-                    gui_main.window[f'Action_ShowHide_{domain}_reports'].update(text=f'Hide reports')
+                    gui_main.window[f"Action_ShowHide_{domain}_reports"].update(text=f"Hide reports")
                 else:
-                    gui_main.window[f'Action_ShowHide_{domain}_reports'].update(text=f'Show reports')
+                    gui_main.window[f"Action_ShowHide_{domain}_reports"].update(text=f"Show reports")
 
-                gui_main.window[f'ShowHide_Item_{domain}'].update(visible=gui_main.showHideStates[domain])
+                gui_main.window[f"ShowHide_Item_{domain}"].update(visible=gui_main.showHideStates[domain])
 
             return None
 
-        gui_main.window[f'ShowHide_Item_{button}'].update(visible=gui_main.showHideStates[button])
+        gui_main.window[f"ShowHide_Item_{button}"].update(visible=gui_main.showHideStates[button])
 
     def loop():
         while True:
